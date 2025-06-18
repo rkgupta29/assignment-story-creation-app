25875a5a0737d6cea1d440ce5d4103a23670475e
rakesh-gupta29 <rakesh.rkgupta29@gmail.com>
2025-06-18 17:42:43 +0530

    chore: quick commit

--- src/components/loaders/full-page.tsx
+++ src/components/loaders/full-page.tsx
@@ -18,8 +18,8 @@ export default function FullPageLoader({
     <div
       className={`min-h-screen flex items-center justify-center ${containerStyles[direction]} gap-2`}
     >
-      <Loader2 className="h-6 w-6 animate-spin" />
-      {text && <p className="text-sm text-gray-600">{text}</p>}
+      <Loader2 className="h-6 w-6 animate-spin text-primary" />
+      {text && <p className="text-base text-primary font-medium">{text}</p>}
     </div>
   );
 }
