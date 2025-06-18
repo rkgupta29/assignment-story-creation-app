1a381922739e253c639a4adf2a177bc6b31a3ebd
rakesh-gupta29 <rakesh.rkgupta29@gmail.com>
2025-06-18 19:38:54 +0530

    chore: quick commit

--- src/components/auth/AuthLayoutWrapper.tsx
+++ src/components/auth/AuthLayoutWrapper.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import BrandLogo from "../BrandLogo";
-import Image from "next/image";
 
 export default function AuthLayoutWrapper({
   children,
@@ -11,12 +10,11 @@ export default function AuthLayoutWrapper({
     <div className=" min-h-screen grid">
       <div className="grid grid-cols-1 md:grid-cols-7 ">
         <div className="relative col-span-3 h-full bg-primary">
-          <Image
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
             src="https://images.unsplash.com/photo-1618120320644-68e7f84030b8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="signup-bg"
-            objectFit="cover"
-            loading="eager"
-            layout="fill"
+            className="w-full h-full object-cover absolute top-0 left-0"
           />
           <div className="absolute inset-0 p-10 flex flex-col justify-between">
             <BrandLogo height={60} width={60} />
